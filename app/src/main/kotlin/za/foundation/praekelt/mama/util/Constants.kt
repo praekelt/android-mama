65a5fb5078ccaaf8eb62170039447f645be7b11b
Eduardo Koloma Jr <eduardo@kolomajr.com>
2015-07-02 23:37:57 +0200

    fix: url patterns in constants file

--- app/src/main/kotlin/za/foundation/praekelt/mama/util/Constants.kt
+++ app/src/main/kotlin/za/foundation/praekelt/mama/util/Constants.kt
@@ -3,20 +3,20 @@ package za.foundation.praekelt.mama.util
 /**
  * Contains constants used throughout the app
  */
-public object Constants{
+public object Constants {
     val BASE_URL:String = "http://172.30.88.49:6543"
-    val REPO_NAME:String = "mama"
+    val REPO_NAME: String = "mama"
 
-    val REST_COMMIT_STUB:String = "{commit_id}"
+    val REST_COMMIT_STUB: String = "commit-id"
 
-    val REMOTE_URL_EXT:String = ".json"
-    val REMOTE_REPOS_DIR:String = "repos"
-    val REMOTE_STATUS_URL:String = "$REMOTE_REPOS_DIR/$REPO_NAME/status$REMOTE_URL_EXT"
-    val REMOTE_CLONE_URL:String = "$REMOTE_REPOS_DIR/$REPO_NAME/clone$REMOTE_URL_EXT"
-    val REMOTE_PULL_URL = "$REMOTE_REPOS_DIR/$REPO_NAME/pull/$REST_COMMIT_STUB/$REMOTE_URL_EXT"
-    val REMOTE_DIFF_URL = "$REMOTE_REPOS_DIR/$REPO_NAME/diff/$REST_COMMIT_STUB/$REMOTE_URL_EXT"
+    val REMOTE_URL_EXT: String = ".json"
+    val REMOTE_REPOS_DIR: String = "/repos"
+    val REMOTE_STATUS_URL: String = "$REMOTE_REPOS_DIR/$REPO_NAME/status$REMOTE_URL_EXT"
+    val REMOTE_CLONE_URL: String = "$REMOTE_REPOS_DIR/$REPO_NAME/clone$REMOTE_URL_EXT"
+    val REMOTE_PULL_URL = "$REMOTE_REPOS_DIR/$REPO_NAME/pull/{$REST_COMMIT_STUB}$REMOTE_URL_EXT"
+    val REMOTE_DIFF_URL = "$REMOTE_REPOS_DIR/$REPO_NAME/diff/{$REST_COMMIT_STUB}$REMOTE_URL_EXT"
 
-    val REMOTE_DATE_FORMAT:String = "yyyy-MM-dd HH:mm:ss"
+    val REMOTE_DATE_FORMAT: String = "yyyy-MM-dd HH:mm:ss"
 
     val FQN_LOCALISATION = "unicore.content.models.Localisation"
     val FQN_CATEGORY = "unicore.content.models.Category"
