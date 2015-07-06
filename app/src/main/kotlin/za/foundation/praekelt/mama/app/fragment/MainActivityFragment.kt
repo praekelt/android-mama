28805d93d74cf6447bbd96c6844f14c65c60d5be
Eduardo Koloma Jr <klmedu001@myuct.ac.za>
2015-07-06 23:32:54 +0200

    refactor: converted MainActivity.java to kotlin, moved MainActivity & MainActivityFragment to new packages

--- app/src/main/kotlin/za/foundation/praekelt/mama/app/fragment/MainActivityFragment.kt
+++ app/src/main/kotlin/za/foundation/praekelt/mama/app/fragment/MainActivityFragment.kt
@@ -1,10 +1,11 @@
-package za.foundation.praekelt.mama
+package za.foundation.praekelt.mama.app.fragment
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import za.foundation.praekelt.mama.R
 
 /**
  * A placeholder fragment containing a simple view.
