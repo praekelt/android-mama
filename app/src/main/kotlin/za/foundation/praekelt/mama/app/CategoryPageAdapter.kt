3c62faaad13bfe13c56391e9ba84e22ef392f869
Eduardo Koloma Jr <eduardo@kolomajr.com>
2015-07-10 19:52:21 +0200

    feat: display published articles for each category
    Created CategoryListFragment and CategoryListAdapter and necessary layouts
    Added the needed values to the colors, dimens and styles resources
    Changed CategoryPageAdapter to return a CategoryListFragment.
    Deleted MainActivityFragment and corresponding
    Created Comparators for ordering Category and Page classes

--- app/src/main/kotlin/za/foundation/praekelt/mama/app/CategoryPageAdapter.kt
+++ app/src/main/kotlin/za/foundation/praekelt/mama/app/CategoryPageAdapter.kt
@@ -1,37 +1,38 @@
 package za.foundation.praekelt.mama.app
 
-import android.database.DataSetObserver
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.util.Log
 import com.raizlabs.android.dbflow.sql.builder.Condition
 import com.raizlabs.android.dbflow.sql.language.Select
-import org.jetbrains.anko.AnkoLogger
 import rx.Observable
 import za.foundation.praekelt.mama.api.model.Category
 import za.foundation.praekelt.mama.api.model.Category_Table
-import za.foundation.praekelt.mama.app.fragment.MainActivityFragment
+import za.foundation.praekelt.mama.app.fragment.CategoryListFragment
+import za.foundation.praekelt.mama.util.CategoryNameComparator
+import za.foundation.praekelt.mama.util.CategoryPositionComparator
+import za.foundation.praekelt.mama.util.OrderBy
 import java.util.ArrayList
-import org.jetbrains.anko.defaultSharedPreferences
 
 /**
  * Adapter for category fragment view pager
  * @see za.foundation.praekelt.mama.app.activity.MainActivity
  */
-class CategoryPageAdapter(val fm: FragmentManager, val locale: String): FragmentPagerAdapter(fm) {
-    init{
-        refreshCategories().subscribe{}
-    }
-
+class CategoryPageAdapter(val fm: FragmentManager, val locale: String,
+                          var orderBy: OrderBy = OrderBy.POSITION):
+        FragmentPagerAdapter(fm) {
     var categories: MutableList<Category> = ArrayList<Category>()
+    init{
+        refreshCategories().subscribe{notifyDataSetChanged()}
+    }
 
     override fun getCount(): Int {
         return categories.size()
     }
 
     override fun getItem(position: Int): Fragment? {
-        return MainActivityFragment()
+        return CategoryListFragment(categories[position].getUuid(), locale)
     }
 
     override fun getPageTitle(position: Int): CharSequence {
@@ -45,6 +46,7 @@ class CategoryPageAdapter(val fm: FragmentManager, val locale: String): Fragment
                         .where(Condition.column(Category_Table.FEATUREDINNAVBAR).`is`(true))
                         .and(Condition.column(Category_Table.LOCALEID).`is`(locale)).queryList()}
                 .filter{list -> list != null}
+                .map{list -> sortList(list)}
                 .map{list ->
                     categories = list
                     Log.i("CPA", "list size = ${categories.size()}")
@@ -55,5 +57,13 @@ class CategoryPageAdapter(val fm: FragmentManager, val locale: String): Fragment
     fun refresh(): Observable<Boolean>{
         return refreshCategories()
     }
+    
+    fun sortList(catList: MutableList<Category>): MutableList<Category>{
+        when(orderBy){
+            OrderBy.POSITION -> return catList.sortBy(CategoryPositionComparator()) as MutableList
+            OrderBy.NAME -> return catList.sortBy(CategoryNameComparator()) as MutableList
+            else -> return catList
+        }
+    } 
 }
 
