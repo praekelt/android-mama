2e28dbbf9dc3582b65570c753a1f4f057ee32d8a
eduardokolomajr <eduardo@kolomajr.com>
2015-08-11 23:18:35 +0200

    test: tested if MainActivity clones and updates from the unicorn repo when necessary

---
+++ app/src/test/java/za/foundation/praekelt/mama/app/MainActivityTest.java
@@ -0,0 +1,58 @@
+package za.foundation.praekelt.mama.app;
+
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import za.foundation.praekelt.mama.BuildConfig;
+import za.foundation.praekelt.mama.api.rest.RestPackage;
+import za.foundation.praekelt.mama.app.activity.MainActivity;
+import za.foundation.praekelt.mama.util.Constants;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link za.foundation.praekelt.mama.app.activity.MainActivity}
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP, application = App.class)
+public class MainActivityTest {
+    ActivityController<MainActivity> activityController;
+    @Before
+    public void setUp(){
+        activityController = Robolectric.buildActivity(MainActivity.class).create();
+    }
+
+    @Test
+    public void testCloneRepo(){
+        SharedPreferences defaultSharedPreferences
+                = PreferenceManager.getDefaultSharedPreferences(activityController.get());
+
+        String defaultCommit = "!!@@##";
+        String commit = defaultSharedPreferences
+                .getString(Constants.SHARED_PREFS_COMMIT, defaultCommit);
+
+        activityController.get().setUcdService(RestPackage.createTestUCDService(RestPackage.createUCDServiceGson()));
+        assertThat(commit).isEqualTo(defaultCommit);
+
+        activityController.resume();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        commit = defaultSharedPreferences
+                .getString(Constants.SHARED_PREFS_COMMIT, defaultCommit);
+
+        assertThat(commit).isNotEqualTo(defaultCommit);
+    }
+}
