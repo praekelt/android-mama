f5d5ef0c141f383f30c1787a7dd4530b507d02b0
eduardokolomajr <eduardo@kolomajr.com>
2015-09-17 11:28:38 +0200

    feat: display page content in activity

--- app/src/main/kotlin/za/foundation/praekelt/mama/app/activity/DetailPageActivity.kt
+++ app/src/main/kotlin/za/foundation/praekelt/mama/app/activity/DetailPageActivity.kt
@@ -1,5 +1,18 @@
 package za.foundation.praekelt.mama.app.activity
 
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import za.foundation.praekelt.mama.R
+import za.foundation.praekelt.mama.app.viewmodel.DetailPageActivityViewModel
+import za.foundation.praekelt.mama.databinding.ActivityDetailPageBinding
+import za.foundation.praekelt.mama.inject.component.DaggerDetailPageActivityComponent
+import za.foundation.praekelt.mama.inject.component.DetailPageActivityComponent
+import za.foundation.praekelt.mama.inject.module.DetailPageActivityModule
+import javax.inject.Inject
+import kotlin.properties.Delegates
+
 /**
  * Created by eduardokolomajr on 2015/09/14.
  */
@@ -10,5 +23,25 @@ public class DetailPageActivity(): AppCompatActivity(), AnkoLogger{
 
     object argsKeys{
         val uuidKey = "pageUuid"
+    }
+
+    var pageUuid: String by Delegates.notNull()
+        @Inject set
+    val activityComp: DetailPageActivityComponent by Delegates.lazy { getDetailPageActivityComponent() }
+    var viewModel: DetailPageActivityViewModel by Delegates.notNull()
+        @Inject set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super<AppCompatActivity>.onCreate(savedInstanceState)
+        activityComp.inject(this)
+        info("after inject ${pageUuid}")
+        val binding: ActivityDetailPageBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail_page)
+        binding.setPage(viewModel)
+    }
+
+    private fun getDetailPageActivityComponent(): DetailPageActivityComponent{
+        return DaggerDetailPageActivityComponent.builder()
+                .detailPageActivityModule(DetailPageActivityModule(this))
+                .build()
     }
 }
---
+++ app/src/main/kotlin/za/foundation/praekelt/mama/inject/component/DetailPageActivityComponent.kt
@@ -0,0 +1,15 @@
+package za.foundation.praekelt.mama.inject.component
+
+import dagger.Component
+import za.foundation.praekelt.mama.app.activity.DetailPageActivity
+import za.foundation.praekelt.mama.inject.module.DetailPageActivityModule
+import za.foundation.praekelt.mama.inject.scope.ActivityScope
+
+/**
+ * Created by eduardokolomajr on 2015/09/14.
+ */
+ActivityScope
+Component(modules = arrayOf(DetailPageActivityModule::class))
+public interface DetailPageActivityComponent{
+    fun inject(activity: DetailPageActivity): Unit
+}
