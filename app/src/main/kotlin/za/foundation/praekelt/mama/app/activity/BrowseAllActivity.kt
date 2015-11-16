25cee90951cc38945c002516295458656e82ce26
Eduardo Koloma Jr <eduardo@kolomajr.com>
2015-11-16 13:11:58 +0200

    feat: added drop down list for categories in browse all activity

--- app/src/main/kotlin/za/foundation/praekelt/mama/app/activity/BrowseAllActivity.kt
+++ app/src/main/kotlin/za/foundation/praekelt/mama/app/activity/BrowseAllActivity.kt
@@ -1,25 +1,67 @@
 package za.foundation.praekelt.mama.app.activity
 
+import android.content.Context
+import android.database.DataSetObserver
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
-import kotlinx.android.synthetic.activity_browse_all.simple_toolbar
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import kotlinx.android.synthetic.activity_browse_all.drawer_layout
 import kotlinx.android.synthetic.activity_browse_all.nav_view
-import org.jetbrains.anko.support.v4.drawerLayout
+import kotlinx.android.synthetic.activity_browse_all.simple_toolbar
+import kotlinx.android.synthetic.activity_browse_all.toolbar_category_spinner
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.layoutInflater
+import org.jetbrains.anko.find
 import za.foundation.praekelt.mama.R
+import za.foundation.praekelt.mama.api.model.Category
+import za.foundation.praekelt.mama.app.viewmodel.BrowseAllActivityViewModel
 
 /**
  * Created by eduardokolomajr on 15/11/10.
  */
 class BrowseAllActivity: AppCompatActivity(), AnkoLogger {
+    val viewModel: BrowseAllActivityViewModel = BrowseAllActivityViewModel(this);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse_all)
         setSupportActionBar(this.simple_toolbar)
         setupNavigationDrawerWithUp(this.drawer_layout, this.nav_view, R.id.nav_browse_all)
+        title = ""
+        viewModel.onAttachActivity(this)
+        this.toolbar_category_spinner.adapter = TSAdapter(viewModel.categories)
     }
+}
+
+class TSAdapter(val categories:List<Category>):
+        BaseAdapter() {
+    override fun getCount(): Int {
+        return categories.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val view:View = convertView ?: parent!!.context.layoutInflater
+                .inflate(R.layout.support_simple_spinner_dropdown_item, parent, false)
+
+        view.find<TextView>(android.R.id.text1).text = categories[position].title
+        return view
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val view:View = convertView ?: parent!!.context.layoutInflater
+                .inflate(R.layout.support_simple_spinner_dropdown_item, parent, false)
+
+        view.find<TextView>(android.R.id.text1).text = categories[position].title
+        return view
+    }
+
+    override fun getItem(position: Int): Category {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong();
+    }
+
 }
---
+++ app/src/main/kotlin/za/foundation/praekelt/mama/app/viewmodel/BrowseAllActivityViewModel.kt
@@ -0,0 +1,30 @@
+package za.foundation.praekelt.mama.app.viewmodel
+
+import com.raizlabs.android.dbflow.sql.builder.Condition
+import com.raizlabs.android.dbflow.sql.builder.ConditionQueryBuilder
+import com.raizlabs.android.dbflow.sql.language.Select
+import org.jetbrains.anko.AnkoLogger
+import za.foundation.praekelt.mama.api.model.Category
+import za.foundation.praekelt.mama.api.model.Category_Table
+import za.foundation.praekelt.mama.app.activity.BrowseAllActivity
+import za.foundation.praekelt.mama.util.SharedPrefsUtil
+import java.util.*
+
+/**
+ * Created by eduardokolomajr on 15/11/16.
+ */
+class BrowseAllActivityViewModel(act: BrowseAllActivity):
+        BaseActivityViewModel<BrowseAllActivity>(act), AnkoLogger{
+    val allCategories: Category = Category().apply { title = "All Categories` " }
+    lateinit var categories: List<Category>
+
+    override fun onAttachActivity(activity: BrowseAllActivity) {
+        super.onAttachActivity(activity)
+        Select().from(Category::class.java)
+                .where(Condition.column(Category_Table.LOCALEID)
+                        .`is`(SharedPrefsUtil.getLocale(this.act!!.get())))
+                .queryList()
+                .sortedBy { it.title }
+                .let { categories = listOf(allCategories)+it }
+    }
+}
